1e4b0d5a96f9c425eedf42ec0cc4d5b9fa7bec2b
Alan Abrahamsen <alan.abrahamsen@gmail.com>
2026-02-15 15:41:45 +1100

    Added docstring for Tile class, and function to check for singleton

--- src/tile.py
+++ src/tile.py
@@ -2,13 +2,25 @@ from domino import Domino
 
 class Tile:
     '''
-
+    Tiles are parts of the gameboard where a domino can be placed. A single Domino spans two Tiles.
+    A Tile contains domains of possible attributes:
+    - neighbours: a set of tiles that may be linked by a domino
+    - dominoes: a set of valid dominoes that may be placed in the tile
+    - values: a set of possible values that the tile may have. This helps track orientation of the domino.
     '''
     def __init__(self, id: str, neighbours: set[str], dominoes: set[Domino]):
         self.id = id
         self.neighbours = neighbours
         self.dominoes = dominoes
         self.values = {v for d in dominoes for v in d.values}
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        if isinstance(other, Tile):
+            return self.id == other.id
+        return NotImplemented
 
     def is_singleton(self, attribute: str = 'all') -> bool:
         valid_attributes = ['neighbours', 'dominoes', 'values', 'all']
